b768604078e9478f2137c55cb52e598fc87871d3
Aishani Mohapatra <mohapatraaishani20@gmail.com>
2023-06-03 23:36:51 +0530

    C++ Files added

---
+++ InvertedRotatedHalfPyramid/Pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    for (int row = 0; row < n; row++) {
+        for (int column = 0; column < row; column++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+} 
